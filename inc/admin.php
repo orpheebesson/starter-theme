7ae009b2e022e53f70f478ddd89bc74e10b3a3ea
Orphée Besson <orphee.besson@etudiant.univ-lr.fr>
2021-04-12 21:36:01 +0200

    up dashboard widgets

--- inc/admin.php
+++ inc/admin.php
@@ -17,3 +17,54 @@ function remove_menus()
 }
 
 add_action('admin_menu', 'remove_menus');
+
+/**
+ * Remove content to the welcome panel on the admin dashboard.
+ *
+ * @return boolean
+ *
+ * @link https://developer.wordpress.org/reference/hooks/welcome_panel/
+ */
+remove_action('welcome_panel', 'wp_welcome_panel');
+
+/**
+ * Removes a meta box from one or more screens.
+ *
+ * @return void
+ *
+ * @link https://developer.wordpress.org/reference/functions/remove_meta_box/
+ */
+function remove_dashboard_widgets()
+{
+    remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
+    remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+    remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+    remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
+    remove_meta_box('dashboard_activity', 'dashboard', 'normal');
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
+    remove_meta_box('dashboard_primary', 'dashboard', 'side');
+}
+
+add_action('wp_dashboard_setup', 'remove_dashboard_widgets');
+
+/**
+ * Adds a new dashboard widget.
+ *
+ * @return void
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_add_dashboard_widget/
+ */
+function add_dashboard_widgets()
+{
+    wp_add_dashboard_widget('welcome_dashboard_widget', 'Bienvenue', 'welcome_dashboard_widget');
+}
+
+add_action('wp_dashboard_setup', 'add_dashboard_widgets');
+ 
+
+function welcome_dashboard_widget()
+{
+    echo "Bienvenue sur ce site.";
+}
